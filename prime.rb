2fc76719a4f6049cd18257e6d0cc7ba33ae570bd
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2020-11-22 01:54:07 -0600

    commit tree da543a41557ae7304fa02a405355eb6d4b42c95d

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+require "pry"
+def prime?(num)
+    lower_num = (1..num-1)
+    result = []
+
+    if (num.abs) == 1 or (num.abs) == 0
+        return false
+    elsif num == 2
+        return true
+    else
+        (2..(num.abs)-1).each do |div_num|
+            result << num % div_num
+        end
+        if result.find {|i| i == 0} 
+            return false
+        else 
+            return true 
+        end
+    end
+    #binding.pry
+end
+
+# binding.pry
